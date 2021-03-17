50936ed01cba759fd1815ddb61265de0313ee9bb
DataHearth <antoine.l@antoine-langlois.net>
2021-03-17 18:09:55 +0100

    use DNS lookup instead of HEAD request

--- pkg/subdomain/main.go
+++ pkg/subdomain/main.go
@@ -2,9 +2,6 @@ package subdomain
 
 import (
 	"net"
-	"net/http"
-	h "net/http"
-	"net/http/httptrace"
 	"strings"
 	"time"
 
@@ -45,54 +42,24 @@ func NewSubdomain(logger logrus.FieldLogger, subdomainAddr string) (Subdomain, e
 
 // RetrieveSubdomainIP will retrieve the subdomain IP with a HEAD request
 func (sd *subdomain) retrieveSubdomainIP() error {
-	var remoteAddr string
-	logger := sd.logger.WithField("component", "retrieve-subdomain-ip")
-
-	// * create HEAD request
-	req, err := http.NewRequest("HEAD", "https://"+sd.subdomainAddr, nil)
+	ips, err := net.LookupIP(sd.subdomainAddr)
 	if err != nil {
 		return err
 	}
-	// * create a trace to get server remote address
-	trace := &httptrace.ClientTrace{
-		GotConn: func(gci httptrace.GotConnInfo) {
-			remoteAddr = gci.Conn.RemoteAddr().String()
-		},
-	}
-	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 
-	// * create a client to perform the request
-	client := new(h.Client)
-	client.Timeout = 5 * time.Second
-
-	// * perform the request
-	resp, err := client.Do(req)
-	if err != nil {
-		// todo: ignoring errors is bad. Implement a solution to scrape 100% of the time remote addr
-		logger.WithError(err).WithFields(logrus.Fields{
-			"subdomain": sd.subdomainAddr,
-		}).Errorln(utils.ErrHeadRemoteIP.Error())
-		sd.ip = ""
-		return nil
-	}
-	if resp.StatusCode != 200 && remoteAddr == "" {
-		logger.WithFields(logrus.Fields{
-			"status-code": resp.StatusCode,
-			"subdomain":   sd.subdomainAddr,
-		}).Errorln(utils.ErrWrongStatusCode.Error())
-		return utils.ErrWrongStatusCode
+	if len(ips) != 1 {
+		return utils.ErrIpLenght
 	}
 
-	// * check if remote address contains a port
-	if strings.Contains(remoteAddr, ":") {
-		remoteAddr, _, err = net.SplitHostPort(remoteAddr)
+	ip := ips[0].String()
+	if strings.Contains(ip, ":") {
+		ip, _, err = net.SplitHostPort(ip)
 		if err != nil {
-			logger.WithError(err).WithField("remote-address", remoteAddr).Errorln(utils.ErrSplitAddr.Error())
 			return utils.ErrSplitAddr
 		}
 	}
 
-	sd.ip = remoteAddr
+	sd.ip = ip
 
 	return nil
 }
